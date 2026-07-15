c96ad5604fb322498ea9d7b004fa02b95ccfd646
Hasib <gudhulibela031@gmail.com>
2026-07-15 23:52:22 +0600

    Add Task 8: Merge Sort Comparison Count

---
+++ Offline assignment/Offline_assignment_3/00724205101098_Offline3_Task8.cpp
@@ -0,0 +1,85 @@
+// 8. Calculate the total number of comparisons made during merge sort.
+
+#include<bits/stdc++.h>
+using namespace std;
+int comp=0;
+void merge(vector<int>&v,int low,int mid,int high)
+{
+    int i=low;
+    int j=mid+1;
+    int k=low;
+    vector<int>temp;
+    while(i<=mid && j<=high)
+    {
+
+        if(v[i]<=v[j])
+        {
+            temp.push_back(v[i]);
+            i++;
+            k++;
+            comp++;
+        }
+        else{
+            temp.push_back(v[j]);
+            j++;
+            k++;
+        }
+        comp++;
+    }
+    if(i>mid)
+    {
+        while(j<=high)
+        {
+            temp.push_back(v[j]);
+            j++;
+            k++;
+        }
+    }
+    else{
+        while(i<=mid)
+    {
+        temp.push_back(v[i]);
+            i++;
+            k++;
+    }
+    }
+    int g=0;
+    for(int i=low;i<=high;i++)
+    {
+        v[i]=temp[g++];
+    }
+
+}
+void mergeSort(vector<int>&v,int low,int high)
+{
+  if(high>low)
+{
+  int mid=(low+high)/2;
+    mergeSort(v,low,mid);
+    mergeSort(v,mid+1,high);
+
+ merge(v,low,mid,high);
+
+}
+
+}
+
+
+int main()
+{
+  int n;
+    cin>>n;
+        vector<int>v;
+        for(int i=0;i<n;i++)
+        {
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }
+        mergeSort(v,0,n-1);
+        for(auto y:v)
+        {
+            cout<<y<<" ";
+        }
+    cout<<endl<<comp;
+}
