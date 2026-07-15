3563d863a1c319321633e86b46d320f383cab560
Hasib <gudhulibela031@gmail.com>
2026-07-15 23:52:17 +0600

    Add Task 3: Quick Sort Swap Count

---
+++ Offline assignment/Offline_assignment_3/00724205101098_Offline3_Task3.cpp
@@ -0,0 +1,69 @@
+//3. Count the number of swaps required to sort an array using quick sort.
+
+#include<bits/stdc++.h>
+using namespace std;
+int Count=0;
+int Sort(vector<int>&v,int high,int low)
+{
+    int pv=v[high];
+    int i=low;
+    int j=high;
+
+    while(i<j){
+      while(i<high && pv>v[i])
+      {
+        i++;
+
+      }
+
+       while(j>low && pv<=v[j])
+       {
+        j--;
+
+        }
+
+    if(i<j)
+    {
+        Count++;
+       swap(v[i],v[j]);
+    }
+
+
+    }
+
+    swap(v[i],v[high]);
+    Count++;
+    return i;
+}
+void quickSort(vector<int>&v,int high,int low)
+{
+
+    if(high>low)
+    {
+        int pt=Sort(v,high,low);
+        quickSort(v,pt-1,low);
+        quickSort(v,high,pt+1);
+    }
+}
+
+int main()
+{
+    int n;
+  vector<int>v;
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+      int x;
+      cin>>x;
+      v.push_back(x);
+
+  }
+
+  quickSort(v,n-1,0);
+  for(auto x:v)
+  {
+      cout<<x<<" ";
+  }
+  cout<<endl<<Count;
+
+}
